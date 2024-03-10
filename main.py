adbf913f6f7bedb2f6ab6ede4d3e26272913cdd7
Daniel Jaouen <dcj24@cornell.edu>
2024-03-10 08:46:21 -0400

    Check for FLASK_DEBUG env var

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 
 import hy
+import os
 from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    if os.getenv('FLASK_DEBUG'):
+        app.run(debug=True)
+    else:
+        app.run(debug=False)
