170f9d2c74820411bf21a4843ccae8b22896480c
Daniel Jaouen <dcj24@cornell.edu>
2024-03-10 08:50:46 -0400

    Formatting

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 from app import app
 
 if __name__ == "__main__":
-    if os.getenv('FLASK_DEBUG'):
+    if os.getenv("FLASK_DEBUG"):
         app.run(debug=True)
     else:
         app.run(debug=False)
